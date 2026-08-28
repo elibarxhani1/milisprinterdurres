be0e2ed546ff2f8f603173d711d49f1a26cce611
elibarxhani1 <eli.barxhani@gmail.com>
2026-08-28 23:27:59 +0200

    Fix static GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   reactStrictMode: true,
   devIndicators: false,
   images: {
-    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+      },
+    ],
   },
 }
 
